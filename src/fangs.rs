29029341eeb8a90288bd3d146a5495ca901b17d1
kana-rus <kanarus786@gmail.com>
2024-04-23 21:54:04 +0900

    improve CSRFang

--- src/fangs.rs
+++ src/fangs.rs
@@ -19,11 +19,11 @@ impl FangAction for LayoutFang {
 pub struct CSRFang;
 impl FangAction for CSRFang {
     async fn fore<'a>(&'a self, req: &'a mut Request) -> Result<(), Response> {
-        let origin = req.headers.Origin();
-        (origin == Some(ORIGIN))
+        let referer = req.headers.Referer();
+        (referer == Some(&format!("{ORIGIN}/")))
             .then_some(())
             .ok_or_else(|| {
-                worker::console_warn!("Unexpected request from {}", origin.unwrap_or("unkown origin"));
+                worker::console_warn!("Unexpected request from {}", referer.unwrap_or("no referer"));
                 Response::Forbidden()
             })
     }
