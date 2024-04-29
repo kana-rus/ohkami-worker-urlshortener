056c762260c8cb14611f502af903ded8cbd07606
kana-rus <kanarus786@gmail.com>
2024-04-29 20:00:08 +0900

    use `#[worker::send]`

--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,6 @@
-use ohkami::{Request, FromRequest};
 use ohkami::{typed::Payload, builtin::payload::URLEncoded};
-use worker::send::{SendFuture, SendWrapper};
-use worker::kv::{KvStore, ToRawKvValue};
-use std::{borrow::Cow, future::Future};
-use crate::{pages, AppError};
+use std::borrow::Cow;
+use crate::pages;
 
 
 pub use pages::IndexPage;
@@ -15,35 +12,3 @@ pub use pages::CreatedPage;
 pub struct CreateShortenURLForm<'req> {
     pub url: Cow<'req, str>,
 }
-
-// pub struct KV(SendWrapper<KvStore>);
-// impl<'req> FromRequest<'req> for KV {
-//     type Error = AppError;
-//     fn from_request(req: &'req Request) -> Option<Result<Self, Self::Error>> {
-//         Some(req.env().kv("KV").map_err(AppError::Worker)
-//             .map(|kv| Self(SendWrapper(kv)))
-//         )
-//     }
-// }
-// impl KV {
-//     pub fn get<'kv>(&'kv self,
-//         key: &'kv str,
-//     ) -> impl Future<Output = Result<Option<String>, AppError>> + Send + 'kv {
-//         SendFuture::new(async move {
-//             self.0.get(key)
-//                 .text().await
-//                 .map_err(AppError::kv)
-//         })
-//     }
-// 
-//     pub fn put<'kv>(&'kv self,
-//         key:   &'kv str,
-//         value: impl ToRawKvValue + 'kv,
-//     ) -> impl Future<Output = Result<(), AppError>> + Send + 'kv {
-//         SendFuture::new(async move {
-//             self.0.put(key, value).unwrap()
-//                 .execute().await.map_err(AppError::kv)
-//         })
-//     }
-// }
-// 
